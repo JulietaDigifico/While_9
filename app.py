8436e3c0e02cdafd513cab3e576c28e8baec66d7
Julieta <julieta.digifico@gmail.com>
2024-02-22 22:29:44 -0300

    Ejercicio9 While

---
+++ app.py
@@ -0,0 +1,73 @@
+import tkinter
+from tkinter.messagebox import showinfo as alert
+from tkinter.messagebox import askyesno as question
+from tkinter.simpledialog import askstring as prompt
+import customtkinter
+
+'''
+nombre: Julieta
+apellido: Digifico
+---
+Ejercicio: while_09
+---
+Enunciado:
+Al presionar el botón ‘Comenzar ingreso’, solicitar mediante prompt todos los números que el usuario quiera 
+hasta que presione el botón Cancelar (en el prompt). 
+Luego determinar el máximo y el mínimo 
+e informarlos en los cuadros de textos txt_maximo y txt_minimo respectivamente
+
+'''
+
+
+class App(customtkinter.CTk):
+
+    def __init__(self):
+        super().__init__()
+
+        self.title("UTN FRA")
+
+        self.txt_minimo = customtkinter.CTkEntry(
+            master=self, placeholder_text="Mínimo")
+        self.txt_minimo.grid(row=0, padx=20, pady=20)
+
+        self.txt_maximo = customtkinter.CTkEntry(
+            master=self, placeholder_text="Máximo")
+        self.txt_maximo.grid(row=1, padx=20, pady=20)
+
+        self.btn_mostrar = customtkinter.CTkButton(
+            master=self, text="Comenzar Ingreso", command=self.btn_comenzar_ingreso_on_click)
+        self.btn_mostrar.grid(row=2, padx=20, pady=20,
+                              columnspan=2, sticky="nsew")
+
+    def btn_comenzar_ingreso_on_click(self):
+
+        contador = 0
+    
+        while(True):
+            numero = prompt("", "Ingrese un numero")
+            if(numero == None):
+                break
+            else:
+                contador += 1
+                numero = int(numero)
+
+            if contador == 1:
+                minimo = numero
+                maximo = numero
+            else:
+                if numero < minimo:
+                    minimo = numero
+                if numero > maximo:
+                    maximo = numero
+                
+        if contador == 0:
+            alert("Mensaje", "No se ingresaron numeros")
+        else:
+            self.txt_maximo.insert(0, maximo)
+            self.txt_minimo.insert(0, minimo)
+
+
+if __name__ == "__main__":
+    app = App()
+    app.geometry("300x300")
+    app.mainloop()
